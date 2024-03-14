fa3dcc90b8a545ac43310f9ad3a4d647ff07bc54
Diana Martseniuk <diana@rectavia.studio>
2024-03-14 12:53:36 +0100

    add loading button while getting form data

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // TODO: add the loading button
+    // Change button to the loading mode
     const button = document.getElementById("submit-button");
     button.disabled = true;
+    button.innerHTML =
+      '<img class="inline mr-3 w-5 h-5 align-text-top text-white animate-spin" src="./public/icons/loading.svg">Submitting...';
 
     // Perform an AJAX request to submit the form
     var xhr = new XMLHttpRequest();
@@ -22,12 +24,14 @@ document
           // Successful response
           var response = xhr.responseText;
           button.disabled = false;
+          button.innerText = "Contact Us";
           console.log(response);
           document.getElementById("sheetdb-form").reset(); //Clear the form fields
         } else {
           // Error response
           console.log(xhr.status);
           button.disabled = false;
+          button.innerText = "Contact Us";
           console.log("Error: Something went wrong."); // Show a generic error message
         }
       }
