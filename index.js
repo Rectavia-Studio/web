cc53d5dc03e538cb1741c5f921a82ec617098e4b
Diana Martseniuk <diana@rectavia.studio>
2024-03-14 13:16:42 +0100

    add resultDiv to show the submission responce

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ document
     button.innerHTML =
       '<img class="inline mr-3 w-5 h-5 align-text-top text-white animate-spin" src="./public/icons/loading.svg">Submitting...';
 
+    // Getting the result div to show the result message to the user
+    const resultDiv = document.getElementById("result");
+    resultDiv.innerText = "";
+
     // Perform an AJAX request to submit the form
     var xhr = new XMLHttpRequest();
     xhr.open("POST", this.action);
@@ -25,14 +29,16 @@ document
           var response = xhr.responseText;
           button.disabled = false;
           button.innerText = "Contact Us";
+          resultDiv.innerText =
+            "Form submission was successful. Thank you. We will get back to you within 24 hours";
           console.log(response);
-          document.getElementById("sheetdb-form").reset(); //Clear the form fields
+          document.getElementById("sheetdb-form").reset();
+          // Form submission was successful. Thank you. We will get back to you within 24 hours
         } else {
           // Error response
-          console.log(xhr.status);
           button.disabled = false;
           button.innerText = "Contact Us";
-          console.log("Error: Something went wrong."); // Show a generic error message
+          console.log("Error: Something went wrong. Please, try again");
         }
       }
     };
